aec71020c3e1e4665d2df3f7d7aeb3e46e609ce5
simonetripodi <simone.tripodi@gmail.com>
2012-02-01 11:21:39 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,10 @@
+var NAVTREE_DATA =
+[ [ "com.terradue.dsione", "com/terradue/dsione/package-summary.html", [ [ "Description", "com/terradue/dsione/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Command", "com/terradue/dsione/Command.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AbstractCommand", "com/terradue/dsione/AbstractCommand.html", null, "" ], [ "DescribeImages", "com/terradue/dsione/DescribeImages.html", null, "" ], [ "DescribeInstances", "com/terradue/dsione/DescribeInstances.html", null, "" ], [ "Register", "com/terradue/dsione/Register.html", null, "" ], [ "RunInstances", "com/terradue/dsione/RunInstances.html", null, "" ], [ "Server", "com/terradue/dsione/Server.html", null, "" ], [ "TerminateInstances", "com/terradue/dsione/TerminateInstances.html", null, "" ], [ "URIConverter", "com/terradue/dsione/URIConverter.html", null, "" ], [ "Upload", "com/terradue/dsione/Upload.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.terradue.dsione.model", "com/terradue/dsione/model/package-summary.html", [ [ "Description", "com/terradue/dsione/model/package-descr.html", null, "" ], [ "Classes", null, [ [ "Appliance", "com/terradue/dsione/model/Appliance.html", null, "" ], [ "Deployment", "com/terradue/dsione/model/Deployment.html", null, "" ], [ "UploadTicket", "com/terradue/dsione/model/UploadTicket.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,18 @@
+var DATA = [
+      { id:0, label:"com.terradue.dsione", link:"com/terradue/dsione/package-summary.html", type:"package" },
+      { id:1, label:"com.terradue.dsione.AbstractCommand", link:"com/terradue/dsione/AbstractCommand.html", type:"class" },
+      { id:2, label:"com.terradue.dsione.Command", link:"com/terradue/dsione/Command.html", type:"class" },
+      { id:3, label:"com.terradue.dsione.DescribeImages", link:"com/terradue/dsione/DescribeImages.html", type:"class" },
+      { id:4, label:"com.terradue.dsione.DescribeInstances", link:"com/terradue/dsione/DescribeInstances.html", type:"class" },
+      { id:5, label:"com.terradue.dsione.Register", link:"com/terradue/dsione/Register.html", type:"class" },
+      { id:6, label:"com.terradue.dsione.RunInstances", link:"com/terradue/dsione/RunInstances.html", type:"class" },
+      { id:7, label:"com.terradue.dsione.Server", link:"com/terradue/dsione/Server.html", type:"class" },
+      { id:8, label:"com.terradue.dsione.TerminateInstances", link:"com/terradue/dsione/TerminateInstances.html", type:"class" },
+      { id:9, label:"com.terradue.dsione.URIConverter", link:"com/terradue/dsione/URIConverter.html", type:"class" },
+      { id:10, label:"com.terradue.dsione.Upload", link:"com/terradue/dsione/Upload.html", type:"class" },
+      { id:11, label:"com.terradue.dsione.model", link:"com/terradue/dsione/model/package-summary.html", type:"package" },
+      { id:12, label:"com.terradue.dsione.model.Appliance", link:"com/terradue/dsione/model/Appliance.html", type:"class" },
+      { id:13, label:"com.terradue.dsione.model.Deployment", link:"com/terradue/dsione/model/Deployment.html", type:"class" },
+      { id:14, label:"com.terradue.dsione.model.UploadTicket", link:"com/terradue/dsione/model/UploadTicket.html", type:"class" }
+
+    ];

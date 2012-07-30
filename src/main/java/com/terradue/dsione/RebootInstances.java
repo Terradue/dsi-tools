e48e91392e1c10baee0fbf6f8c7dc425047c004b
simonetripodi <simone.tripodi@gmail.com>
2012-07-30 09:52:25 +0200

    initial checkin of RebootInstances class

---
+++ src/main/java/com/terradue/dsione/RebootInstances.java
@@ -0,0 +1,40 @@
+package com.terradue.dsione;
+
+/*
+ *  Copyright 2012 Terradue srl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import org.kohsuke.MetaInfServices;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@MetaInfServices
+@Parameters( commandDescription = "Reboot an instance of a particular image." )
+public final class RebootInstances
+    extends BaseTool
+{
+
+    @Parameter( names = { "-H", "--headers" }, description = "Display column headers" )
+    private boolean headers = false;
+
+    @Override
+    public void execute()
+        throws Exception
+    {
+        // TODO
+    }
+
+}

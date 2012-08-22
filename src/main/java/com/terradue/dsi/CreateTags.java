595f699b342ae7e869e5bbdb0b0d34c2859feb94
simonetripodi <simone.tripodi@gmail.com>
2012-08-22 10:09:18 +0200

    implemented the CreateTags command

--- src/main/java/com/terradue/dsi/CreateTags.java
+++ src/main/java/com/terradue/dsi/CreateTags.java
@@ -18,7 +18,15 @@ package com.terradue.dsi;
 
 import static java.lang.System.exit;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.inject.name.Named;
+import com.sun.jersey.api.client.UniformInterfaceException;
 
 @Parameters( commandDescription = "Creates an instance snapshot" )
 public final class CreateTags
@@ -30,11 +38,41 @@ public final class CreateTags
         exit( new CreateTags().execute( args ) );
     }
 
+    @Parameter( description = "The instances identificator(s)" )
+    protected List<String> ids = new LinkedList<String>();
+
+    @Inject
+    @Override
+    public void setServiceUrl( @Named( "service.deployments" ) String serviceUrl )
+    {
+        super.setServiceUrl( serviceUrl );
+    }
+
     @Override
     public void execute()
         throws Exception
     {
-        // TODO
+        for ( String id : ids )
+        {
+            logger.info( "Creating SNAPSHOT for instance {}", id );
+
+            try
+            {
+                restClient.resource( new StringBuilder( serviceUrl )
+                                    .append( '/' )
+                                    .append( id )
+                                    .append( "/snapshotStorage" )
+                                    .toString() )
+                          .post();
+
+                logger.info( "SNAPSHOT for instance {} successfully created", id );
+            }
+            catch ( UniformInterfaceException e )
+            {
+                logger.warn( "Impossible to create a snapshot for instance {}: {}",
+                             id, e.getResponse().getClientResponseStatus().getReasonPhrase() );
+            }
+        }
     }
 
 }

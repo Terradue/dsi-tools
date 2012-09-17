a25063af848f902073090973010fc04d4a7f5a37
simonetripodi <simone.tripodi@gmail.com>
2012-09-17 16:17:05 +0200

    initial checkin of a command to enlist all available users

---
+++ src/main/java/com/terradue/dsi/ListUsers.java
@@ -0,0 +1,57 @@
+package com.terradue.dsi;
+
+/*
+ *  Copyright 2012 Terradue srl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import static java.util.Arrays.asList;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.sun.jersey.api.client.GenericType;
+import com.terradue.dsi.model.Account;
+
+/**
+ * @since 0.2
+ */
+public final class ListUsers
+    extends AbstractDescribeCommand
+{
+
+    @Override
+    @Inject
+    public void setServiceUrl( @Named( "service.accounts" ) String serviceUrl )
+    {
+        super.setServiceUrl( serviceUrl );
+    }
+
+    @Override
+    protected List<String> getDefaultFields()
+    {
+        return asList( "id", "name", "owner" );
+    }
+
+    @Override
+    protected void execute()
+        throws Exception
+    {
+        log( restClient.resource( serviceUrl ).get( new GenericType<Collection<Account>>(){} ) );
+    }
+
+}

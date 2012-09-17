f07d8d02255fc0f825d8375e95a25be0cd08dad7
simonetripodi <simone.tripodi@gmail.com>
2012-09-17 16:13:33 +0200

    initial checkin of Account entity

---
+++ src/main/java/com/terradue/dsi/model/Account.java
@@ -0,0 +1,88 @@
+package com.terradue.dsi.model;
+
+/*
+ *  Copyright 2012 Terradue srl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * @since 0.2
+ */
+@XmlAccessorType( FIELD )
+public final class Account
+{
+
+    @XmlElement( name = "accountId" )
+    private String id;
+
+    @XmlElement( name = "accountName" )
+    private String name;
+
+    @XmlElement( name = "userAccountOwner" )
+    private AccountOwner owner;
+
+    /**
+     * @return the id
+     */
+    public String getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId( String id )
+    {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return the owner
+     */
+    public AccountOwner getOwner()
+    {
+        return owner;
+    }
+
+    /**
+     * @param owner the owner to set
+     */
+    public void setOwner( AccountOwner owner )
+    {
+        this.owner = owner;
+    }
+
+}

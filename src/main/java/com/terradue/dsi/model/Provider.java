d7ee7d4a1fb5c4ef6ac71f9f6eb572b24f5e1e0c
simonetripodi <simone.tripodi@gmail.com>
2012-11-14 13:24:44 +0100

    added new tool to list providers

---
+++ src/main/java/com/terradue/dsi/model/Provider.java
@@ -0,0 +1,164 @@
+package com.terradue.dsi.model;
+
+/*
+ *  Copyright 2012 Terradue srl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType( FIELD )
+@XmlRootElement( name = "provider" )
+public final class Provider
+{
+
+    @XmlAttribute
+    private String id;
+
+    @XmlElement
+    private String address;
+
+    @XmlElement
+    private String email;
+
+    @XmlElement( name = "managingDirector" )
+    private String manager;
+
+    @XmlElement
+    private String name;
+
+    @XmlElement( name = "shortDesc" )
+    private String description;
+
+    @XmlElement( name = "websiteUrl" )
+    private String url;
+
+    /**
+     * @return the id
+     */
+    public String getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId( String id )
+    {
+        this.id = id;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress()
+    {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress( String address )
+    {
+        this.address = address;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail()
+    {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail( String email )
+    {
+        this.email = email;
+    }
+
+    /**
+     * @return the manager
+     */
+    public String getManager()
+    {
+        return manager;
+    }
+
+    /**
+     * @param manager the manager to set
+     */
+    public void setManager( String manager )
+    {
+        this.manager = manager;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl()
+    {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl( String url )
+    {
+        this.url = url;
+    }
+
+}
